9d39bc04002c85e70b8cf64ed314b9efd81874d9
Chang-Young Park <parkcy0630@gmail.com>
2023-07-10 22:54:30 +0900

    update

--- Team_AI/All/002) Image/dataset_220907_last_addFilterTest/human/jpg.bat
+++
@@ -1,9 +0,0 @@
-@echo off 
-setlocal
-setlocal EnableDelayedExpansion
-for %%a in (*.jpg) do (
-set f=%%a
-set f=!f:^(=!
-set f=!f:^)=!
-ren "%%a" "!f!"
-)
--- Team_AI/All/002) Image/파일명_공백_괄호_제거_관련/txt.bat
+++
@@ -1,9 +0,0 @@
-@echo off 
-setlocal
-setlocal EnableDelayedExpansion
-for %%a in (*.txt) do (
-set f=%%a
-set f=!f:^(=!
-set f=!f:^)=!
-ren "%%a" "!f!"
-)
